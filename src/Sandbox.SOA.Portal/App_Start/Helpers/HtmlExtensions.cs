3f75ed5189f629c31adbf5a58ebf6985eed55733
Anthony Johnston <aj@antix.co.uk>
2014-01-25 10:26:22 +0000

    quick resources investigation

---
+++ src/Sandbox.SOA.Portal/App_Start/Helpers/HtmlExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Resources;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sandbox.SOA.Portal.Helpers
+{
+    public class ResourceManagerEx:ResourceManager
+    {
+        public ResourceManagerEx(string name, Assembly assembly):
+            base(name, assembly)
+        {
+            
+        }
+
+        public IHtmlString GetHtmlString(string name, System.Globalization.CultureInfo culture)
+        {
+            var value = string.Format("<resource>{0}</resource>", base.GetString(name, culture));
+
+            return MvcHtmlString.Create(value);
+        }
+    }
+}
