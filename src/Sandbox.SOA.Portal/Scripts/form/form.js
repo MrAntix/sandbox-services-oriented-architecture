a2eaa02b5e3b93ff75eeecb36811ad123ca6392a
Anthony Johnston <aj@antix.co.uk>
2014-02-02 20:49:38 +0000

    use single quotes

--- src/Sandbox.SOA.Portal/Scripts/form/form.js
+++ src/Sandbox.SOA.Portal/Scripts/form/form.js
@@ -1,12 +1,12 @@
 ﻿;
 (function($) {
-    "use strict";
+    'use strict';
 
-    var invalid = "has-error";
+    var invalid = 'has-error';
 
     $(function () {
 
-        $("[type='tel']").tel({debug:true});
+        $('[type="tel"]').tel({debug:true});
         $('select').selectpicker();
 
         //alert($.fn.tel.format("44 07899 793042"));
@@ -14,16 +14,16 @@
         //alert($.fn.tel.format("[gb]44"));
         //alert($.fn.tel.format("[dk]44 (0)7899 793042"));
 
-        $("body").on(
-            "submit", "form[method='POST']",
+        $('body').on(
+            'submit', 'form[method="POST"]',
             function() {
 
                 var $form = $(this),
                     data = $form.serialize();
 
                 $.ajax({
-                    url: $form.attr("action"),
-                    type: $form.attr("method"),
+                    url: $form.attr('action'),
+                    type: $form.attr('method'),
                     data: data
                 })
 
@@ -31,40 +31,43 @@
                         var response;
 
                         $form
-                            .find(":input")
-                            .popover("destroy")
+                            .find(':input')
+                            .popover('destroy')
                             .removeClass(invalid);
 
                         if (xhr.status === 400) {
                             response = JSON.parse(xhr.responseText);
                             var fields = Object.keys(response);
 
-                            $form.find(".validation-summary")
-                                .addClass("alert alert-danger")
-                                .empty();
+                            $form.find('.validation-summary')
+                                .addClass('alert alert-danger')
+                                .empty()
+                                .append('<ol/>');
 
                             fields.forEach(function(field) {
                                 if (response[field].Errors && response[field].Errors.length > 0) {
                                     var message = response[field].Errors[0].ErrorMessage,
                                         $field = $form
-                                            .find("[name='" + field + "']");
+                                            .find('[name="' + field + '"]');
 
                                     $field
                                         .popover({
                                             content: message,
-                                            trigger:"focus",
-                                            placement: "auto top",
-                                            template: "<div class='popover popover-invalid'><div class='arrow'></div><h3 class='popover-title'></h3><div class='popover-content'></div></div>",
+                                            trigger:'focus',
+                                            placement: 'auto top',
+                                            template: '<div class="popover popover-invalid"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>',
                                             container: $form
                                         })
                                         .addClass(invalid);
 
-                                    var $label = $("<label/>")
-                                        .attr("for", $field.attr("id"))
+                                    var $label = $('<label/>')
+                                        .attr('for', $field.attr('id'))
                                         .text(message);
 
-                                    $form.find(".validation-summary")
-                                        .append($("<p class='col-sm-offset-2 col-sm-10'/>").append($label));
+                                    $form.find('.validation-summary ol')
+                                        .append(
+                                                $('<li/>').append($label)
+                                        );
                                 }
                             });
                         }
